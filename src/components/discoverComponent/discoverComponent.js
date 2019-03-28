59bdea1431a3dd10921b2bd364fc43aa7b46d9e0
Suraj Sharma <www.surajsharma@gmail.com>
2019-03-28 11:23:46 +0530

    carousel done

--- src/components/discoverComponent/discoverComponent.js
+++ src/components/discoverComponent/discoverComponent.js
@@ -1,61 +1,46 @@
 import React from 'react';
+
 import AliceCarousel from 'react-alice-carousel';
 import "react-alice-carousel/lib/alice-carousel.css";
+
 import './discoverComponent.css' 
+
+
 export default class Gallery extends React.Component {  
-  responsive = {
-    0: { items: 1 },
-    600: { items: 2 },
-    1024: { items: 4 },
-  };
-  
-  onSlideChange(e) {
-    console.log('Item`s position during a change: ', e.item);
-    console.log('Slide`s position during a change: ', e.slide);
-  };
- 
-  onSlideChanged(e) {
-    console.log('Item`s position after changes: ', e.item);
-    console.log('Slide`s position after changes: ', e.slide);
-  };
-  
-  galleryItems() {
-    return (
-      [ 
+  items = [
       <img src='https://bit.ly/2OwcNt6' alt='alt'/>,
       <img src='https://bit.ly/2OwcNt6' alt='alt'/>,
       <img src='https://bit.ly/2OwcNt6' alt='alt'/>,
       <img src='https://bit.ly/2OwcNt6' alt='alt'/>,
       <img src='https://bit.ly/2OwcNt6' alt='alt'/>
-      ].map((item, i) => (
-        <div key={`key-${i}`} className="card carcard">
-          <h2>{item}</h2>
-        </div>
+  ]
+
+  state = {
+    galleryItems: this.items.map((i) => (
+      <div className='card carcard' key={i}>{i}
+        <p> Ballyhoo </p>
+      </div>
       ))
-    )
-  };
-  
-  render() {
-    const items = this.galleryItems();
- 
+  }
+
+  render() { 
     return (
-      <AliceCarousel
-        items={items}
-        duration={400}
-        autoPlay={false}
-        startIndex = {1}
-        fadeOutAnimation={true}
-        mouseDragEnabled={true}
-        playButtonEnabled={false}
-        autoPlayInterval={2000}
-        autoPlayDirection="rtl"
-        responsive={this.responsive}
-        disableAutoPlayOnAction={true}
-        onSlideChange={this.onSlideChange}
-        onSlideChanged={this.onSlideChanged}
-        dotsDisabled={true}
-        buttonsDisabled={false}
-      />
+    <div>
+      <div className='carousel-btns'>
+        <button className='prevbtn' onClick={() => this.Carousel._slidePrev()}>⮃</button>
+        <button className='nextbtn' onClick={() => this.Carousel._slideNext()}>⮁</button>    
+      </div>
+      <div className='carousel'>
+        <AliceCarousel
+            dotsDisabled={true}
+            buttonsDisabled={true}
+            items={this.state.galleryItems}
+            ref={(el) => (this.Carousel = el)}   
+            responsive={{ 1024: {items:4} }}   
+            />
+      </div>
+    </div>
+
     );
   }
 }

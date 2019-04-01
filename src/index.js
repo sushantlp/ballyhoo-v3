51fce47e7295c4d2f3766c5d958471d52e0198c5
Suraj Sharma <www.surajsharma@gmail.com>
2019-04-01 11:34:30 +0530

    hot reloading, popular zoom

--- src/index.js
+++ src/index.js
@@ -4,7 +4,19 @@ import './index.css';
 import App from './app';
 import * as serviceWorker from './serviceWorker';
 
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
+//HOT RELOADING
+//https://medium.com/@brianhan/hot-reloading-cra-without-eject-b54af352c642
+
+if(module.hot){
+    module.hot.accept('./app', ()=> {
+        const NextApp = require('./app').default
+        ReactDOM.render(<NextApp />, document.getElementById('root'))
+    })
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

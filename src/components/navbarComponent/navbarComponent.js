38dceb91426de237e0be8b99d3b769bedd8c66da
Suraj Sharma <www.surajsharma@gmail.com>
2019-04-01 15:26:30 +0530

    fixed navbar menu position

--- src/components/navbarComponent/navbarComponent.js
+++ src/components/navbarComponent/navbarComponent.js
@@ -6,29 +6,29 @@ export default class Navbar extends React.Component{
         return(
             <div>
                 <div className='navbar has-text-centered columns'>
-                    <div className='brandname column is-1'>
+                    <div className='brandname column'>
                         <h1>BALLYHOO</h1>
                     </div>
-                    <div className='nav_searchbar column is-6'>
+                    <div className='nav_searchbar column is-5'>
                        <select className='nav_dropdown'>{this.props.options}</select>
                        &nbsp;
                        <input  type="text" className='nav_search' placeholder='search' required/>
                     </div>
-                    <div className='column is-1'>
+                    <div className='column'>
                         <i class="fa fa-user-circle"><p className='nav_text'>Sign in</p></i>
                     </div>
                 </div>
-                <div className='nav_menu columns is-10'>
-                    <div className='column'>
-                        <p className='nav_menu_text'>MENU</p>       
+                <div className='nav_menu columns'>
+                    <div className='column'/>
+                    <div className='column is-5 has-text-centered'>
+                        <p className='nav_menu_text'>Menu</p>       
                         <p className='nav_menu_text'>Discover</p>       
                         <p className='nav_menu_text'>Featured</p>       
                         <p className='nav_menu_text'>Popular</p>       
                         <p className='nav_menu_text'>Trending</p>
+                        <p className='nav_menu_text'>Download App</p>
                     </div>
-                    <div className='column is-2'>
-                        <p className='nav_menu_right'>Download App</p>
-                    </div>
+                    <div className='column'/>
                 </div>
             </div>
             )

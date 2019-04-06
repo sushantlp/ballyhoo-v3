5fa0bbeaec7ac61de73473f2b1d858a70a3fd192
Suraj Sharma <www.surajsharma@gmail.com>
2019-04-06 16:45:28 +0530

    Mcards done

--- src/components/listComponent/listComponent.js
+++ src/components/listComponent/listComponent.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import './listComponent.css'
-
+import { Link } from "react-router-dom";
 
 export default  class List extends Component {
-    items = [
-        <img src='http://bit.ly/2U4qwwZ' width={265} alt='alt'/>,
-        <img src='http://bit.ly/2U0MtNs' width={265} alt='alt'/>,
-        <img src='http://bit.ly/2FAUIGc' width={265} alt='alt'/>,
-        <img src='http://bit.ly/2FzZmnM' width={265} alt='alt'/>,
-        <img src='http://bit.ly/2FA5xIy' width={265} alt='alt'/>,
-        <img src='http://bit.ly/2U4qwwZ' width={265} alt='alt'/>,
-        <img src='http://bit.ly/2U0MtNs' width={265} alt='alt'/>,
-        <img src='http://bit.ly/2FAUIGc' width={265} alt='alt'/>,
-        <img src='http://bit.ly/2FzZmnM' width={265} alt='alt'/>,
-        <img src='http://bit.ly/2FA5xIy' width={265} alt='alt'/>
+    images = [
+        'http://bit.ly/2U4qwwZ',
+        'http://bit.ly/2U0MtNs',
+        'http://bit.ly/2FAUIGc',
+        'http://bit.ly/2FzZmnM',
+        'http://bit.ly/2FA5xIy',
+        'http://bit.ly/2U4qwwZ',
+        'http://bit.ly/2U0MtNs',
+        'http://bit.ly/2FAUIGc',
+        'http://bit.ly/2FzZmnM',
+        'http://bit.ly/2FA5xIy'
     ]
 
     render() {
@@ -21,27 +21,51 @@ export default  class List extends Component {
             <div className='listcontainer columns'>
                 <div className='column is-2'>
                 </div>
+                
                 <div style={{"marginLeft":"15px"}} className='column is-8'>
-                <section style={{"marginLeft":"10px"}}>
-                    <h2 className='subtitle'>Merchant List</h2>
-                    <nav class="breadcrumb" aria-label="breadcrumbs">
-                      <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/">Discover</a></li>
-                        <li class="is-active"><a href="/">Merchants</a></li>
-                      </ul>
-                    </nav>
-                </section>
-                    {this.items.map((i)=> 
-                        <div className='card mcard cardinline'>{i}
-                            <p>&nbsp;Merchant</p>
-                            <p style={{"fontSize":"0.8em"}}>&nbsp;Locality<br/></p>
-                            <p style={{"fontSize":"0.8em"}}>&nbsp;Info<br/></p>
-                            <p style={{"fontSize":"0.6em"}} className='hashtag tag is-info'>#Hashtag</p>
-                            <p style={{"fontSize":"0.6em"}} className='hashtag tag is-info'>#Hashtag</p>
-                            <p style={{"fontSize":"0.6em"}} className='hashtag tag is-info'>#Hashtag</p><br/>
-                            <p style={{"fontSize":"0.6em"}} className='tag is-warning'>Rating: 5.5</p>
+                    <section style={{"marginLeft":"10px"}}>
+                        <h2 className='subtitle'>Merchant List</h2>
+                        <nav class="breadcrumb" aria-label="breadcrumbs">
+                          <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/">Discover</a></li>
+                            <li class="is-active"><a href="/">Merchants</a></li>
+                          </ul>
+                        </nav>
+                    </section>
 
+                    {this.images.map((i)=> 
+                        <div className='card mcard cardinline'>                            
+                                <img src={i} width={265} alt='alt' />
+
+                                <p style={{"float":"right","fontSize":"0.6em"}} className='tag is-warning'>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star-half"></i>
+                                </p>
+
+                                <Link>
+                                    <p>&nbsp;Merchant</p> 
+                                    <p style={{"fontSize":"0.8em"}}>&nbsp;Locality<br/></p>
+                                    <p style={{"fontSize":"0.8em"}}>&nbsp;Offer Title<br/></p>
+                                </Link>
+
+                                <div className='hashtags'>
+                                    <p style={{"fontSize":"0.6em"}} className='hashtag tag is-info'>#Hashtag</p>
+                                    <p style={{"fontSize":"0.6em"}} className='hashtag tag is-info'>#Hashtag</p>
+                                    <p style={{"fontSize":"0.6em"}} className='hashtag tag is-info'>#Hashtag</p>
+                                    <p style={{"fontSize":"0.6em"}} className='hashtag tag is-info'>#Hashtag</p>
+                                    <br/><hr/>
+                                </div>
+
+                                <div className='pnb'>
+                                    <p style={{"float":"right"}}>Price:&nbsp;
+                                        <i class="fas fa-rupee-sign"></i> 1000 onwards
+                                    </p>
+                                    <button className='tag is-danger'>BOOK</button>
+                                </div>
                         </div>
                     )}
                 </div>
